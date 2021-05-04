e85c5240241a0fb317ddc37ee24b27790356ba13
Matthew Gonzales <gonzalesmattg@gmail.com>
2021-05-03 20:48:48 -0500

    enabled private routes with react-router-dom

--- src/helpers/Routes.js
+++ src/helpers/Routes.js
@@ -1,27 +1,46 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import Home from '../views/Home';
 import Author from '../views/Authors';
 import AddAuthor from '../views/AddStudent';
 import SingleAuthor from '../views/SingleAuthor';
 
-export default function Routes({ authors, setAuthors }) {
+// The PrivateRoute function is creating a private route and returing the specified route based on the props
+// We specify the specific props we want to use in the routeChecker and pass the rest with the spread
+const PrivateRoute = ({ component: Component, user, ...rest }) => {
+  // when we call this function in the return, it is looking for an argument. `props` here is taco.
+  const routeChecker = (taco) => (user
+    ? (<Component {...taco} user={user} />)
+    : (<Redirect to={{ pathname: '/', state: { from: taco.location } }} />));
+    // this render method is one we can use instead of component. Since the components are being dynamically created, we use render. Read the docs for more info: https://reactrouter.com/web/api/Route/render-func
+  // Just like in the routes if we want the dynamically rendered component to have access to the Router props, we have to pass `props` as an argument.
+  return <Route {...rest} render={(props) => routeChecker(props)} />;
+};
+
+PrivateRoute.propTypes = {
+  component: PropTypes.func,
+  user: PropTypes.any
+};
+export default function Routes({ user, authors, setAuthors }) {
   return (
     <div>
       <Switch>
         <Route exact path='/' component={Home} />
-        <Route
+        <PrivateRoute
           exact path='/authors'
+          user={user}
           component={() => <Author authors={authors} setAuthors={setAuthors} />}
         />
-        <Route
+        <PrivateRoute
           path='/authors/:firebaseKey'
           component={SingleAuthor}
+          user={user}
         />
-        <Route
+        <PrivateRoute
           path='/add-author'
           component={() => <AddAuthor setAuthors={setAuthors} />}
+          user={user}
         />
       </Switch>
     </div>
@@ -30,5 +49,6 @@ export default function Routes({ authors, setAuthors }) {
 
 Routes.propTypes = {
   authors: PropTypes.array.isRequired,
-  setAuthors: PropTypes.func.isRequired
+  setAuthors: PropTypes.func.isRequired,
+  user: PropTypes.any
 };
